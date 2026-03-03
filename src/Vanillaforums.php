220797b2bc1b7defd8b04429755b33a305ac67c9
Phil Richards <phil@phrichards.ca>
2026-03-03 14:42:00 -0500

    Add type to schemaVersion

--- src/Vanillaforums.php
+++ src/Vanillaforums.php
@@ -50,7 +50,7 @@ class Vanillaforums extends Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '1.0.0';
+    public string $schemaVersion = '1.0.0';
 
     // Public Methods
     // =========================================================================
